47251680f7d25e6a1ebd9b9a8b169ac23813fc83
Deyvis <deyvisnvg@gmail.com>
2020-07-30 10:59:43 -0500

    Envío de Datos del arduino al Servidor promedio de temperatura y humedad segundo intento

--- src/public/js/monitoreo.js
+++ src/public/js/monitoreo.js
@@ -13,13 +13,18 @@ const socketio = io();
 socketio.on("sensores", datos => {
     console.log(datos)
 
-    temperatura1.innerHTML = datos.temperatura1;
-    humedad1.innerHTML = datos.humedad1;
+    temp1 = parseInt(datos.temperatura1);
+    hum1 = parseInt(datos.humedad1);
+    temp2 = parseInt(datos.temperatura2);
+    hum2 = parseInt(datos.humedad2);
 
-    temperatura2.innerHTML = datos.temperatura2;
-    humedad2.innerHTML = datos.humedad2;
+    //----------------------------------------------------------------//
 
-    proTemperatura.innerHTML = (datos.temperatura1 + datos.temperatura2)/2
+    temperatura1.innerHTML = temp1;
+    humedad1.innerHTML = hum1;
+    temperatura2.innerHTML = temp2;
+    humedad2.innerHTML = hum2;
 
-    proHumedad.innerHTML = (datos.humedad1 + datos.humedad2)/2
+    proTemperatura = (temp1 + temp2) / 2;
+    proHumedad = (hum1 + hum2) / 2;
 })
