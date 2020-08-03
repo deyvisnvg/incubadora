cc90f4f6cd238155920b94fa6880b2d481b4884f
Deyvis <deyvisnvg@gmail.com>
2020-08-02 20:43:19 -0500

    probando

--- src/components/monitoreo/network.js
+++ src/components/monitoreo/network.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const asyncify = require('express-asyncify');
+var moment = require('moment');
 
 const router = asyncify(express.Router());
 
@@ -39,6 +40,26 @@ router.get('/', secure.checkOwn, (req, res) => {
     req.session.message = "";
 
     let fecha = new Date()
+
+    var localDate = new Date();
+    var localMoment = moment();
+    var utcMoment = moment.utc();
+    var utcDate = new Date(utcMoment.format());
+
+    //These are all the same
+    console.log('localData unix = ' + localDate.valueOf());
+    console.log('localMoment unix = ' + localMoment.valueOf());
+    console.log('utcMoment unix = ' + utcMoment.valueOf());
+
+    //These formats are different
+    console.log('localDate = ' + localDate);
+    console.log('localMoment string = ' + localMoment.format());
+    console.log('utcMoment string = ' + utcMoment.format());
+    console.log('utcDate  = ' + utcDate);
+
+    //One to show conversion
+    console.log('localDate as UTC format = ' + moment.utc(localDate).format());
+    console.log('localDate as UTC unix = ' + moment.utc(localDate).valueOf());
 
     res.render('links/monitoreo', { fecha, user });
 
